d1dfcf0369310d2ee215ebbed6b1f20ef5473eb3
rishav <ghisingrishav@gmail.com>
2024-03-17 20:28:43 +0545

    versioning

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './Features/store.ts'
-console.log("%c9GUFF \n build-2024 mar 11\t \t v0.8", "background: black; color: white; display: block;padding:20px");
+console.log("%c9GUFF \n build-2024 mar 17\t \t v0.9", "background: black; color: white; display: block;padding:20px");
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <BrowserRouter>
